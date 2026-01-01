d87d1aaee1d5f8754288d59fbb4bfe40ca02aa7e
Levi-Gaeming <Levihickey30@icloud.com>
2026-01-01 01:35:02 -0500

    Update B64-B32.sh

--- B64-B32.sh
+++ B64-B32.sh
@@ -1,3 +1,10 @@
+# dependencies
+echo "Installing Dependencies"
+sudo apt update
+sudo apt install -y git cmake make gcc g++ python3 pkg-config binutils \
+    libc6 libc6-dev libstdc++6 libstdc++6-dev libpthread-stubs0-dev \
+    libgl1-mesa-dev libvulkan-dev binfmt-support
+
 # this clones box64
 echo -n "Cloning Box64"
 git clone https://github.com/ptitSeb/box64 &>/dev/null
