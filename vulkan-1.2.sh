9d9a8404d3f745b6549b6c736a335c110ca212fe
Levi-Gaeming <Levihickey30@icloud.com>
2025-12-31 23:35:21 -0500

    Update vulkan-1.2.sh

--- vulkan-1.2.sh
+++ vulkan-1.2.sh
@@ -1,15 +1,25 @@
-# this script is just taken from the l4t discord server lol
+#!/bin/bash
+set -e
 
-# update all installed packages
+# this updates
+echo "Updating"
 sudo apt update
-sudo apt dist-upgrade
+sudo apt dist-upgrade -y
 
-# add the support for the noble-32-7 dist
+# this adds support for the BSP
+echo "Adding support for the new BSP"
 echo "deb [signed-by=/etc/apt/keyrings/theofficialgman-L4T.asc] https://theofficialgman.github.io/l4t-debs/ l4t noble-32-7" | sudo tee /etc/apt/sources.list.d/theofficialgman-L4T-32-7.list
 
-# remove BSP version restrictions
+# this removes restrictions
+echo "Removing BSP restrictions"
 sudo rm -f /etc/apt/preferences.d/00-switch-bsp-restrictions
 
-# update and install new nvidia-bsp-32-7 meta package
+# this updates again and installs the new BSP
+echo "Installing the new BSP"
 sudo apt update
 sudo apt install -y -o Dpkg::Options::="--force-confdef" nvidia-bsp-32-7
+
+# auto reboot
+echo "Rebooting in 10 seconds"
+sleep 10
+sudo reboot
